c2704de836f58d06f72b06d10baf40901bbad773
Jon Kuperman <jon.kuperman@gmail.com>
2016-07-21 14:02:13 -0700

    moved to new boilerplate

--- js/main.js
+++
@@ -1,7 +0,0 @@
-const ipcRenderer = require('electron').ipcRenderer;
-
-ipcRenderer.sendSync('synchronous-message');
-
-ipcRenderer.on('asynchronous-reply', function(event, arg) {
-  console.log(arg);
-});
